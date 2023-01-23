27357eb90dcabaa52503b793dc056b7de934e2cd
Anthony-ndegwa-dev <Anthony.n.ndegwa@gmail.com>
2023-01-23 04:24:32 -0800

    Add prototype functions to header file

--- sort.h
+++ sort.h
@@ -27,5 +27,8 @@ void _qsort(int *a, int low, int high, int size);
 void shell_sort(int *array, size_t size);
 void _ssort(int *a, int size, int n);
 void cocktail_sort_list(listint_t **list);
+void decrease_sort(listint_t **ptr, listint_t **limit, listint_t **list);
+void increase_sort(listint_t **ptr, listint_t **limit, listint_t **list);
+void swap_list(listint_t **ptr1, listint_t **ptr2, int n);
 
 #endif
